f9fb470ff9398b374e9d73c2d1407274941973b1
khiluck <59829939+khiluck@users.noreply.github.com>
2020-04-28 17:03:59 +0300

    Add files via upload

---
+++ dwm_download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /usr/src
+
+git clone git://git.suckless.org/dwm
+git clone git://git.suckless.org/st
+git clone git://git.suckless.org/dmenu
+
+# replace config.h files in directories and compile it with
+#
+# sudo make clean install
+#
+#
