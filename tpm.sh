b5b134d720a07097f8b21e99d4c769c5a95f54f9
khiluck <59829939+khiluck@users.noreply.github.com>
2020-12-11 11:47:50 +0200

    Update tpm.sh

--- tpm.sh
+++ tpm.sh
@@ -14,3 +14,13 @@ openssl req -newkey rsa:2048 -nodes -keyout db.key -new -x509 -sha256 -days 3650
 openssl x509 -outform DER -in db.crt -out db.cer
 cert-to-efi-sig-list -g "$(< GUID.txt)" db.crt db.esl
 sign-efi-sig-list -g "$(< GUID.txt)" -k KEK.key -c KEK.crt db db.esl db.auth
+
+
+
+objcopy \
+	--add-section .osrel=/etc/os-release --change-section-vma .osrel=0x20000 \
+	--add-section .cmdline=cmdline.txt --change-section-vma .cmdline=0x30000 \
+	--add-section .linux="/boot/vmlinuz" --change-section-vma .linux=0x40000 \
+	--add-section .initrd="/boot/initrd.img" --change-section-vma .initrd=0x3000000 \
+	/usr/lib/systemd/boot/efi/linuxx64.efi.stub /boot/EFI/BOOT/BOOTX64.EFI
+sbsign --key ${SBKEYS}/db.key --cert ${SBKEYS}/db.crt --output /boot/EFI/BOOT/BOOTX64.EFI /boot/EFI/BOOT/BOOTX64.EFI
