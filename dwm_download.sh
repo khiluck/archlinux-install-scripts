49765799be6768506917242c7c875c5a2a09c966
khiluck <khiluck@gmail.com>
2021-03-01 10:35:14 +0200

    add scroll and slock

--- dwm_download.sh
+++ dwm_download.sh
@@ -5,6 +5,9 @@ cd /usr/src
 git clone git://git.suckless.org/dwm
 git clone git://git.suckless.org/st
 git clone git://git.suckless.org/dmenu
+git clone git://git.suckless.org/scroll
+git clone git://git.suckless.org/slock
+
 
 # replace config.h files in directories and compile it with
 #
